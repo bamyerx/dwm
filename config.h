5894d988f5f5d6aa2d92983873c062c0df8b01bb
alex <bamyerx@gmail.com>
2026-06-10 21:49:03 -0700

    Switched over fully from Rofi to dmenu

--- config.h
+++ config.h
@@ -6,9 +6,9 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { 
-   "FiraCode Nerd Font:size=10" 
+   "Source Serif 4 Caption Regular:size=11" 
 };
-static const char dmenufont[]       = "FiraCode Nerd Font:size=10";
+static const char dmenufont[]       = "Source Serif 4 Caption Regular:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -36,7 +36,6 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-/* static const char *tags[] = { "", "󰊯", "", "", "󰙯", "󱇤", "", "󱘶", "󰧮" }; */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -44,9 +43,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class       instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",      NULL,       NULL,       0,            1,           -1 },
+   { "feh",       NULL,       NULL,       0,            1,           -1 },
+	{ "librewolf", NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
@@ -58,10 +58,10 @@ static const int refreshrate = 120;  /* refresh rate (per second) for client mov
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-   { "III",      horizontal },
-	{ "[]",        tile },    /* first entry is default */
-	{ "[M]",      monocle },
-	{ "><>",        NULL }    /* no layout function means floating behavior */
+   { "",      horizontal },
+	{ "",        tile },    /* first entry is default */
+	{ "󰊓",      monocle },
+	{ "",        NULL }    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -77,17 +77,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_org, "-sf", col_bg, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *scrnshotcmd[] = { "sh", "-c", "maim -s | xclip -selection clipboard -t image/png", NULL };
-static const char *rofi[] = {"rofi", "-show", "drun" };
-static const char *pdfcmd[] = { "/home/alex/.local/bin/pdf-rofi", "/home/alex/books", NULL };
+static const char *pdfcmd[] = { "/home/alex/.local/bin/pdf-dmenu.sh", "/home/alex/books", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = scrnshotcmd } },
-	{ MODKEY,                       XK_d,      spawn,          {.v = rofi } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
    { MODKEY,                       XK_p,      spawn,          {.v = pdfcmd } },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -147,4 +146,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
